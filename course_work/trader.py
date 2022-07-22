43eb7e0768fe9a7d40ce0b403226571cfb58fd40
Bezvladlen <bezvladlen@gmail.com>
2022-07-22 17:24:41 +0300

    all done (I hope)

---
+++ course_work/trader.py
@@ -0,0 +1,140 @@
+import json
+import random
+import argparse
+
+system_file = 'system.json'
+
+
+def get_data():
+    """
+    Получение данных из системного файла
+    """
+    with open(system_file, 'r') as json_file:
+        data = json.load(json_file)
+    return data
+
+
+def system_change(data):
+    """
+    Запись в системный файл измененных данных
+    """
+    with open(system_file, 'w') as json_file:
+        json.dump(data, json_file)
+
+
+def rate(data):
+    """
+    Получение текущего курса UAH к USD
+    """
+    print(data['exchange_rate'])
+
+
+def available(data):
+    """
+    Текуший баланс счета
+    """
+    print(f"USD {data['USD']}\n"
+          f"UAH {data['UAH']}")
+
+
+def buy_usd(usd_quantity, data):
+    """
+    Покупка указанной суммы USD
+    """
+    recount = round(usd_quantity * data['exchange_rate'], 2)
+    if (data['UAH'] - recount) > 0:
+        data['UAH'] = round((data['UAH'] - recount), 2)
+        data['USD'] = round((data['USD'] + usd_quantity), 2)
+        system_change(data)
+        print(f"YOU SOLD {usd_quantity} USD")
+    else:
+        print(f"UNAVAILABLE, REQUIRED BALANCE UAH {recount}, AVAILABLE {data['UAH']}")
+
+
+def sell_usd(usd_quantity, data):
+    """
+    Продажа указанной суммы USD
+    """
+    if usd_quantity <= data['USD']:
+        recount = usd_quantity * data['exchange_rate']
+        data['UAH'] = round((data['UAH'] + recount), 2)
+        data['USD'] = round((data['USD'] - usd_quantity), 2)
+        system_change(data)
+        print(f"YOU BOUGHT {usd_quantity} USD")
+    else:
+        print(f"UNAVAILABLE, REQUIRED BALANCE USD {usd_quantity}, AVAILABLE {data['USD']}")
+
+
+def buy_max_usd(data):
+    """
+    Покупка USD на все UAH, которые доступны на счете
+    """
+    if data['UAH'] > 0.01 * data['exchange_rate']:
+        amount_of_currency = round(data['UAH'] / data['exchange_rate'], 2)
+        data['USD'] = round(data['USD'] + amount_of_currency, 2)
+        data['UAH'] = round(data['UAH'] - amount_of_currency * data['exchange_rate'], 2)
+        print(f"YOU BOUGHT {amount_of_currency} USD")
+        if data['UAH'] < 0:
+            data['UAH'] = round((data['UAH'] + 0.01 * data['exchange_rate']), 2)
+            data['USD'] = round(data['USD'] - 0.01, 2)
+        system_change(data)
+
+
+def sell_all_usd(data):
+    """
+    Вродажа всех USD которые доступны на счете
+    """
+    if data['USD'] > 0:
+        amount_of_currency = data['USD'] * data['exchange_rate']
+        data['USD'] -= data['USD']
+        data['UAH'] = round(data['UAH'] + amount_of_currency, 2)
+        system_change(data)
+        print(f"YOU SOLD {amount_of_currency} USD")
+
+
+def next_day(data):
+    """
+    Рандомная смена курса валют в указанном диапазоне и выдача нового курса
+    """
+    exchange_rate = data['exchange_rate']
+    data['exchange_rate'] = round(random.uniform(exchange_rate - data['delta'], exchange_rate + data['delta']), 2)
+    system_change(data)
+
+
+def restart():
+    """
+    Перезагрузка системы, вовзращение системного файла к стоковой конфигурации
+    """
+    with open('config.json', 'r') as json_file:
+        data = json.load(json_file)
+    system_change(data)
+    return data
+
+
+args = argparse.ArgumentParser()
+args.add_argument('command')
+args.add_argument('amount', type=str, nargs='?', default=0)
+args = vars(args.parse_args())
+command = args['command']
+amount = args['amount']
+
+if command == 'NEXT':
+    next_day(get_data())
+elif command == 'RATE':
+    rate(get_data())
+elif command == 'AVAILABLE':
+    available(get_data())
+elif command == 'RESTART':
+    restart()
+elif command == 'BUY':
+    if amount == 'ALL':
+        buy_max_usd(get_data())
+    else:
+        buy_usd(float(amount), get_data())
+elif command == 'SELL':
+    if amount == 'ALL':
+        sell_all_usd(get_data())
+    else:
+        sell_usd(float(amount), get_data())
+else:
+    print('COMMAND NOT RECOGNIZED')
